ef60429bec0f569cd671d1b24b776c0bb569bb95
Fernando Rivera <frivieravillalobos@guardiaschool.fr>
2023-02-20 11:00:24 +0100

    switch to procedural

--- scrap.py
+++ scrap.py
@@ -1,16 +1,30 @@
 #import random
 import random
 
-#def the cells
+#define the cells
+"""
 class Cell(object):
     position = (int,int)
     life = bool
 def __init__(xy, yesno):
     self.position = xy
-    self.life = yesno
-
+    self.life = yesno 
+"""
 
 
 #create the Grid
+wide = 10
+tall = 10
+grille = []
+row = []
+cell = "."
+for i in range(wide):
+    row[i] = cell
+for i in range(tall):
+    grille.append(row)
+
+
+
+
 
 #code the rules
